00a4adb8610839226b84e6782a1457665cbf3e78
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-08 14:39:05 -0700

    Correct file 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %lu", sizeof(char));
-	printf("Size of an int: %lu", sizeof(int));
-	printf("Size of a long int: %lu", sizeof(long int));
-	printf("Size of a long long int: %lu", sizeof(long long int));
-	printf("Size of a float: %lu", sizeof(float));
+	printf("Size of a char: %lu\n", sizeof(char));
+	printf("Size of an int: %lu\n", sizeof(int));
+	printf("Size of a long int: %lu\n", sizeof(long int));
+	printf("Size of a long long int: %lu\n", sizeof(long long int));
+	printf("Size of a float: %lu\n", sizeof(float));
 	return (0);
 }
