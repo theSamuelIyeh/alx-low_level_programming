51e45d3f6bddd1edc951d77b60a022aa7a8b94dc
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-09 09:10:41 -0700

    Create 6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print single digit of base 10
+ * without using type char
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+	return (0);
+}
