74d4c6c798c8902828447e4a41343d552a589057
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-09 07:53:34 -0700

    Create file 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all lower case alphabet
+ *
+ * Return 0
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (ch <= z)
+	{
+		putchar("%c", letter);
+		letter++;
+	}
+	return (0);
+}
