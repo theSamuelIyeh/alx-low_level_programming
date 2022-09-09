1751d5c80e674374dcec041d28d9737774e492ec
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-09 11:22:46 -0700

    Correct file 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,8 +15,11 @@ int main(void)
 	for (number = 0; number < 10; number++)
 	{
 		putchar(number % 10 + '0');
-		putchar(',');
-		putchar(' ');
+		if (number != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
