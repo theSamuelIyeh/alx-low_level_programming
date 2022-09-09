defce85c0ee6526c6eeaf395e412f179bba8a251
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-09 10:50:03 -0700

    Create file 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print numbers of base 16 in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number < 16)
+	{
+		putchar(number % 16 + '0');
+	}
+	putchar('\n');
+	return (0);
+}
