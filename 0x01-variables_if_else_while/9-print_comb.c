aeb10adde0b45f469b00b852f4ab516825860e72
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-09 11:20:04 -0700

    Create file 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print every possible combination of single
+ * digits seperated by comma and space
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar(number % 10 + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
