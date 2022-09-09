884a23701e238144bdb2d6c7b783aa327b9090e2
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-09 12:20:24 -0700

    Create file 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all possible different combination of
+ * two digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int number1 = 0;
+	int number2 = 0;
+
+	while (number1 < 10)
+	{
+		putchar(number1 % 10 + '0');
+		if (number2 == number1 || number1 > number2)
+		{
+			number2++;
+		}
+		else
+		{
+			if (number2 > 9)
+			{
+				number2 = 0;
+				number1++;
+			}
+			else
+			{
+				putchar(number2 % 10 + '0');
+			}
+		}
+		if (number1 == 8 && number2 == 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
