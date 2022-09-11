def7ed18b6ae738116652553471803d63dbd1523
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-11 12:31:54 -0700

    Another commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,41 +12,28 @@ int main(void)
 {
 	int num1 = 0;
 	int num2 = 0;
-	int num3 = 0;
-	int num4 = 0;
 
-	while (num1 < 10)
+	while (num1 < 100)
 	{
 		num2 = 0;
-		while (num2 < 10)
+		while (num2 < 100)
 		{
-			num3 = 0;
-			while (num3 < 10)
+			if (!(num1 >= num2))
 			{
-				num4 = 0;
-				while (num4 < 10)
+				putchar(num1 / 10 + '0');
+				putchar(num1 % 10 + '0');
+				putchar(' ');
+				putchar(num2 / 10 + '0');
+				putchar(num2 % 10 + '0');
+				if (num1 == 98 && num2 == 99)
 				{
-					if (!(num1 >= num2 || num1 >= num3 || num1 >= num4
-					|| num2 >= num3 || num2 >= num4 || num3 >= num4))
-					{
-						putchar(num1 % 10 + '0');
-						putchar(num2 % 10 + '0');
-						putchar(' ');
-						putchar(num3 % 10 + '0');
-						putchar(num4 % 10 + '0');
-						if (num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9)
-						{
-							break;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-					num4++;
+					break;
 				}
-				num3++;
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 			num2++;
 		}
