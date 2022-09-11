46d45fbd1d0b1a710c469956f3a48244839d1c62
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-11 08:52:34 -0700

    Create file 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all possible different combination of three digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int number1 = 0;
+	int number2 = 0;
+	int number3 = 0;
+
+	while (number1 < 7)
+	{
+		while (number2 == number3 || number2 > number3)
+		{
+			number3++;
+		}
+		while (number1 == number2 || number1 > number2)
+		{
+			number2++;
+		}
+		while (number1 == number3 || number1 > number3)
+		{
+			number3 = number2 + 1;
+		}
+		if (number3 > 9)
+		{
+			number3 = 1;
+			number2++;
+		}
+		if (number2 > 9)
+		{
+			number2 = 1;
+			number1++;
+		}
+		putchar(number1 % 10 + '0');
+		putchar(number2 % 10 + '0');
+		putchar(number3 % 10 + '0');
+		
+		if (!(number1 == 7 && number2 == 8 && number3 == 9))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		number3++;
+	}
+	putchar('\n');
+	return (0);
+}
