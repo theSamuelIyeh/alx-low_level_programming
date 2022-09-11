690a45d69ec0def8ab83ad18a8fa872a710a8213
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-11 11:32:13 -0700

    Correct file 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,7 @@ int main(void)
 			number3 = 0;
 			while (number3 < 10)
 			{
-				while (!(a >= c || b >= c || a >= b))
+				while (!(number1 >= number3 || number2 >= number3 || number1 >= number2))
 				{
 					putchar(number1 % 10 + '0');
 					putchar(number2 % 10 + '0');
