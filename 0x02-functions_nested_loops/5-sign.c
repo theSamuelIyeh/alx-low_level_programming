1be91da3287cec803027ef7c71092e237bdee2b1
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-14 14:03:48 -0700

    Another commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of a number
+ *
+ * @n: number of which its sign should be
+ * printed
+ *
+ * Return: 1 if number is positive
+ * 0 if number is zero and
+ * -1 if number is negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		ret = 1;
+	}
+	else if (n = 0)
+	{
+		ret = 0;
+	}
+	else if (n < 0)
+	{
+		ret = -1;
+	}
+	return (ret);
+}
