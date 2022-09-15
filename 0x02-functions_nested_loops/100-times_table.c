0f87b75c7a8dadcc6c3ddc6556c8a42ee3007c56
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-15 08:23:02 -0700

    Another commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,49 +14,54 @@ void print_times_table(int n)
 	int b = 0;
 	int c = 0;
 
-	while (a <= n)
+	if (n > 15 || n < 0)
+	{}
+	else
 	{
-		b = 0;
-		c = 0;
-		while (b <= n)
+		while (a <= n)
 		{
-			if (c / 10 == 0)
+			b = 0;
+			c = 0;
+			while (b <= n)
 			{
-				if (b == 0)
-				{}
-				else
+				if (c / 10 == 0)
 				{
-					_putchar(' ');
-					_putchar(' ');
-				}
-			}
-			else
-			{
-				if (c / 10 > 9)
-				{
-					_putchar((c / 10) / 10 + '0');
-					_putchar((c / 10) % 10 + '0');
+					if (b == 0)
+					{}
+					else
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
 				}
 				else
 				{
-					_putchar(' ');
-					_putchar(c / 10 + '0');
+					if (c / 10 > 9)
+					{
+						_putchar((c / 10) / 10 + '0');
+						_putchar((c / 10) % 10 + '0');
+					}
+					else
+					{
+						_putchar(' ');
+						_putchar(c / 10 + '0');
+					}
 				}
+				_putchar(c % 10 + '0');
+				if (b == n)
+				{
+					break;
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				c += a;
+				b++;
 			}
-			_putchar(c % 10 + '0');
-			if (b == n)
-			{
-				break;
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			c += a;
-			b++;
+			_putchar('\n');
+			a++;
 		}
-		_putchar('\n');
-		a++;
 	}
 }
