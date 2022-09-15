b665986aa638e4f5d8372fd835ce6584a8b05b7c
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-14 21:29:15 -0700

    Another commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - print all natural numbers starting
+ * from given number in parameter list to 98
+ *
+ * @n: given number to start printing from
+ *
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+		if (n == 98)
+		{
+			break;
+		}
+		else
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
