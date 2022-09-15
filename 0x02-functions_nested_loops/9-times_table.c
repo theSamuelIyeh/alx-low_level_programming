5e1e0a966476ee0d41165c0eb04c06956cdbb731
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-14 20:09:53 -0700

    Another commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table starting with
+ * 0
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	while (a <= 10)
+	{
+		b = 0;
+		c = 0;
+		while (b <= 10)
+		{
+			_putchar(c % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			c += a;
+			b++;
+		}
+		a++;
+	}	
+}
