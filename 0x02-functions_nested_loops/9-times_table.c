b38a18ccf09810970b01b6f93c25806229390236
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-14 20:32:40 -0700

    Another commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 				_putchar(c / 10 + '0');
 			}
 			_putchar(c % 10 + '0');
-			if (c % 9 != 0)
+			if (c / a != 9 || c == 0)
 			{
 				_putchar(',');
 				if (c / 10 + '0' == 0)
