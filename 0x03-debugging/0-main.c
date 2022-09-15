462322c3a65b6ab1ceb51d74e205b41808c1d45e
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-15 13:51:08 -0700

    Another commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mian - test whether the function positive_or_negative
+ * gives the correct output
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
