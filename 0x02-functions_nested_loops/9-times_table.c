e4fb6fddadca7ef0f982be4fee553535a9f6187e
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-14 20:15:10 -0700

    Another commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,13 +12,13 @@ void times_table(void)
 	int b = 0;
 	int c = 0;
 
-	while (a <= 10)
+	while (a < 10)
 	{
 		b = 0;
 		c = 0;
-		while (b <= 10)
+		while (b < 10)
 		{
-			_putchar(c % 10 + '0');
+			_putchar(c + '0');
 			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
