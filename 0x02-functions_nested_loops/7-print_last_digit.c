de61d19fe459bd689b5d56c41f79fa9021da5110
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-14 18:58:50 -0700

    Another commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ *
+ * @l: digit of whichlast digit is to be printed
+ *
+ * Return: the last digit of l
+ */
+int print_last_digit(int l)
+{
+	int ret;
+
+	ret = l % 10;
+	return (ret);
+}
