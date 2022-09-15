f879cfd46c7bb2787e03ffc816235a5a73ba5b11
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-14 21:43:22 -0700

    Another commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,35 +10,41 @@
  */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n <= 98)
 	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-		if (n == 98)
+		while (n <= 98)
 		{
-			break;
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			if (n == 98)
+			{
+				break;
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n++;
 		}
-		else
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		n++;
 	}
-	while (n >= 98)
+	else
 	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-		if (n == 98)
+		while (n > 98)
 		{
-			break;
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			if (n == 98)
+			{
+				break;
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n--;
 		}
-		else
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		n--;
 	}
 	_putchar('\n');
 }
