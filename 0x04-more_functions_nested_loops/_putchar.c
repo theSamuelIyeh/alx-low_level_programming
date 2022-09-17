03a9a77f15e13bcaa52feeb5dba097d1e47d6c40
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-17 14:52:56 -0700

    Another commit

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - print single characters out
+ *
+ * @c: character to print out
+ *
+ * Return: void
+ */
+
+void _putchar(int c)
+{
+	write(1, &c, 1);
+}
