3c7ea60c7aa02b4dd27d8ecfc45749e6b695d325
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-17 15:51:44 -0700

    Another commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,15 +15,7 @@ void more_numbers(void)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			if (n / 10 == 0)
-			{
-				_putchar(n % 10 + '0');
-			}
-			else
-			{
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
+			_putchar(n + '0');
 		}
 		_putchar('\n');
 	}
