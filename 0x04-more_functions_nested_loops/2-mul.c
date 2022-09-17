f125f275e8a70b4cd8477b074ac06e790c3514c5
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-17 14:43:14 -0700

    Another commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiply two digits
+ *
+ * @a: first digit to be multiplied
+ * @b: second digit to be multiplied
+ *
+ * Return: answer of multiplication
+ */
+
+int mul(int a, int b)
+{
+	int ret;
+
+	ret = a * b;
+	return (ret);
+}
