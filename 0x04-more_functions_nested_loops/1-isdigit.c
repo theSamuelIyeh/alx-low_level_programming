5c3df7a5d20468940b2f62ee2b6b0ee695a41e75
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-17 14:36:33 -0700

    Create file 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isdigit - chcks for a digit between 0 and 9
+ *
+ * @c: digit to be checked
+ *
+ * Return: 1 if c is a digit or 0 if otherwise
+ */
+
+int _isdigit(int c)
+{
+	int ret;
+
+	if (c >= 48 && c <= 59)
+	{
+		ret = 1;
+	}
+	else
+	{
+		ret = 0;
+	}
+	return (ret);
+}
