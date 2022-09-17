9861240157d3044eff647b4aba911849bd3b0acd
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-17 14:18:09 -0700

    Another commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a letter is uppercase
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if c is uppercase and 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	int ret;
+
+	if (c >= 65 || c<= 90)
+	{
+		ret = 1;
+	}
+	else
+	{
+		ret = 0;
+	}
+	return (ret);
+}
