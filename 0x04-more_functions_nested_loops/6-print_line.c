534fe60cf162feba05ec36d9f6a5c38c3a5662d0
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-17 18:05:11 -0700

    Another commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line on the terminal
+ *
+ * @c: number of times line is drawn
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (n == 0 || n < 0)
+		{
+			break;
+		}
+		else
+			continue;
+		_putchar('-');
+		i++;
+	}
+	_putchar('\n');
+}
