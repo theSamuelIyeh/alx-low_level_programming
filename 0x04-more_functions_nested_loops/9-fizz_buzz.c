7f36c15caf7737e6450d91f85e598a3cfa30cee7
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-19 08:14:26 -0700

    Another commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -24,7 +24,10 @@ int main(void)
 		}
 		else if (num % 5 == 0)
 		{
-			printf("Buzz ");
+			if (num != 100)
+				printf("Buzz ");
+			else
+				printf("Buzz");
 		}
 		else if ((num % 3 == 0) && (num % 5 == 0))
 		{
