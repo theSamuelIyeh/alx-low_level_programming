df546fd33c6a0a536c9007d4bec807bb0228ceb8
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-20 13:59:51 -0700

    Another commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98 - reset a pointer value to 98
+ *
+ * @*n: pointer variable
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# 0x0-pointers arrays and strings
