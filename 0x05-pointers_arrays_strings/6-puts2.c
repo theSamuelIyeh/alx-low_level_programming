206016fa095ce9f92175082e102e6190816bc0ec
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-20 16:33:54 -0700

    Another commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - print every other character of a string
+ * @str: string parameter
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i += 2;
+	}
+}
