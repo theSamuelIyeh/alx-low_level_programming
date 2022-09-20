ddd34cf635028df1a6058cfed3bf20ea5791ee70
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-20 16:25:26 -0700

    Another commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	_puts(s);
+
+	print_rev(s);
+}
