907f2af5891bfdb6e3d96e12fc44b5d4e2784a4c
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-20 17:13:12 -0700

    Another commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,18 @@
-#inc
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - prints a string
+ * @str: string parameter
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int len = _strlen(str);
+
+	for (i = ((len - 1) / 2) + 1; i < len; i++)
+		putchar(*(str + i));
+	putchar(10);
+}
