d6a7e067fd773b3778f1d4c027521cd6966e0f6c
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-20 17:23:59 -0700

    Another commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,17 @@
 
 void rev_string(char *s)
 {
-	_puts(s);
+	int i;
+	int tmp;
+	int len = 0;
 
-	print_rev(s);
+	while (*(s + len) != '\0')
+		len++;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = *(s + i);
+		*(s + i) = *(s + len - i - 1);
+		*(s + len - i - 1) = tmp;
+	}
 }
