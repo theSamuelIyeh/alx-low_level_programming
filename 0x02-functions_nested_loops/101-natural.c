bfe54dc6e44a2b911deb102e9ce6f2b697e44462
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-20 17:51:56 -0700

    Another commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 0;
+	int sum = 0;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if (num % 3 == 0 || num % 5 == 0)
+		{
+			sum += num;
+		}
+	}
+	printf("%d\n", sum);
+	
+	return (0);
+}
