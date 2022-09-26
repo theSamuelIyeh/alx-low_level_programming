08f2163a6dc2d3498c0aa842822ee30097119650
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-09-26 15:22:00 -0700

    Another commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _memcpy - copies memory area
  * @dest: pointer to destination memory area
@@ -10,9 +9,9 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 		dest[i] = src[i];
 	return (dest);
 }
