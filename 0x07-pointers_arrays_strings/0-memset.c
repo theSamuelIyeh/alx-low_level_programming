c0317683c7833b8decdb449db51b09c264c260db
theSamuelIyeh <105016275+theSamuelIyeh@users.noreply.github.com>
2022-09-26 22:21:46 +0100

    Another commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,10 +2,10 @@
 
 /**
  * _memset - fills memory with a constant byte
- * @s: pointer to memory
+ * @s: pointer to memory area 's'
  * @b: constant byte to fill
  * @n: number of bytes to fill
- * Return: pointer to memory
+ * Return: pointer to memory area 's'
  */
 
 char *_memset(char *s, char b, unsigned int n)
