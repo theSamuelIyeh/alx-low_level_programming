e2f67caf32f00d585375f8014a28fca77f2b77c2
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-10-02 08:39:13 -0700

    another commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts-recursion - prints a string followed by a new line
+ * _puts_recursion - prints a string followed by a new line
  * @s: the string to be printed
  */
 
