7513ab601190143c28ba91fec9f0da5b44c2b472
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-10-03 00:45:16 -0700

    Another commit

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,7 +5,7 @@
  * @n: the number to find the factorial of
  *
  * Return: if n > 0 - the factorial of n
- * 	   if n < 0 - -1 to indicate an error
+ * if n < 0 - -1 to indicate an error
  */
 
 int factorial(int n)
@@ -17,5 +17,5 @@ int factorial(int n)
 	else if (n < 0)
 		return (-1);
 	result = n * factorial(n - 1);
-	return result;
+	return (result);
 }
