8dd00a0eeb42534ec93e3a9e47d0d6c908dab2c0
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-10-03 00:42:22 -0700

    Another commit

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,10 +10,12 @@
 
 int factorial(int n)
 {
-	int result = n;
+	int result;
 
-	if (n < 0)
-		return (-1);
-	else if (n >= 0 && n <= 1)
+	if (n == 0)
 		return (1);
+	else if (n < 0)
+		return (-1);
+	result = n * factorial(n - 1);
+	return result;
 }
