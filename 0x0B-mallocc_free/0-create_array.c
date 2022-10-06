d9a861bb5a654d84d35a1ea42c6bbad388d081ed
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-10-06 04:26:46 -0700

    Another commit

---
+++ 0x0B-mallocc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create an array of chars and initialize it
+ * with a specific character
+ * @size: size
+ * @c: character
+ * Return: null if size is 0, pointer or null if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *ptr;
+
+	if (size == 0)
+		return (NULL);
+	ptr = (char *)malloc(size * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		*(ptr + i) = c;
+	return (ptr);
+}
