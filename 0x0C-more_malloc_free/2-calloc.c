e2ab1f0594ed390c60aa2c422a4ed0079439ef0e
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-10-07 21:44:11 -0700

    Another commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocate memory for an array
+ * @nmemb: number of elements
+ * @size: size of each element
+ * Return: pointer to memory, if fail return null
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	int i;
+
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		ptr[i] = 0;
+	return (ptr);
+}
