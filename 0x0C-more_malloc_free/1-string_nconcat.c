8881a48620f10d6af8f4ef5bdcd3fbffadb85c51
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-10-07 21:37:39 -0700

    Another commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,9 +19,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (length1 = 0; s1[length1] != '\0'; length1++)
+	for (length1 = 0; *(s1 + length1) != '\0'; length1++)
 		;
-	for (length2 = 0; s2[length2] != '\0'; length2++)
+	for (length2 = 0; *(s2 + length2) != '\0'; length2++)
 		;
 
 	if (n < length2)
