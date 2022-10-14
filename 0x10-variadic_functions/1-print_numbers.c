d4eb390deae440852334b653da40da13b9d88aa3
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-10-13 20:18:32 -0700

    Another commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers -print numbers
+ * @separator: string printed between numbers
+ * @n: number of variadic parameters
+ * @...: elipsis for variable parameters
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list, int));
+		if (i != n-1)
+		{
+			if (separator != NULL)
+				printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
