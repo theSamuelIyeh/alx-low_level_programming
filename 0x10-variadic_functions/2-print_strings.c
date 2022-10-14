1484825fb41f03633881c222d14801adcd5cab3b
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-10-13 20:42:09 -0700

    Another commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,6 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", va_arg(list, char *));
 		if (separator != NULL)
 			printf("%s", separator);
-		printf("\n");
 	}
+	printf("\n");
+	va_end(list);
 }
