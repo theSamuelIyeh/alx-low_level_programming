492cf88b117fefc5e7a2b76599bfb9f5c0ae73ae
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-10-14 00:41:28 -0700

    Another commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(list, n);
 
-	for (i = 0, i< n; i++)
+	for (i = 0; i < n; i++)
 	{
 		if (va_arg(list, char *) == NULL)
 			printf("nil");
