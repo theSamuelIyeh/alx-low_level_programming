75131209bd2f90b8c7cf5545a95c0ac9455da5b0
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-10-25 13:33:05 -0700

    Another commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 /**
  * main - entry point
  *
@@ -12,5 +14,47 @@
 
 int main(int argc, char *argv[])
 {
+	int sum = 0, i, j, lenstr;
+
+	if (argc <= 1)
+		printf("%d\n", 0)
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			lenstr = strlen(argv[i]);
+			for (j = 0; j < lenstr; j++)
+			{
+				switch (argv[i][j])
+				{
+					case '1':
+						break;
+					case '2':
+						break;
+					case '3':
+						break;
+					case '4':
+						break;
+					case '5':
+						break;
+					case '6':
+						break;
+					case '7':
+						break;
+					case '8':
+						break;
+					case '9':
+						break;
+					case '0':
+						break;
+					default:
+						printf("Error\n");
+						return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
 	return (0);
 }
