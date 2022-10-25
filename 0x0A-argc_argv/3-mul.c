a9a71372eb0474e697ea8b077090902eda1015b6
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-10-25 06:09:01 -0700

    Another commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * main - entry point
  *
@@ -12,5 +12,17 @@
 
 int main(int argc, char *argv[])
 {
+	int mul;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 	return (0);
 }
