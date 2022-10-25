aa4c6951039d5b41c2a3b3fd4fce67e9e032bec4
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-10-25 05:31:12 -0700

    Another commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: number of command line arguments
+ *
+ * @argv: array of strings from command line
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
