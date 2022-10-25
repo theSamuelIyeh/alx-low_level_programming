e3ffb075e4b1858d19583a0910be82b4163d7c90
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-10-25 05:44:02 -0700

    Another commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: number of command line arguments
+ *
+ * @argv: array of command line arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: number of command line arguments
+ *
+ * @argv: array of command line arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	return (0);
+}
