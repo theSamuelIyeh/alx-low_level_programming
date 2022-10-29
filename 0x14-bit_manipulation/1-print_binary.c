d3cbc17fe87b0afc854332ef8de467d126a1c4f1
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-10-28 18:09:39 -0700

    Another commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary value of int
+ *
+ * @n: int to be taken in
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int startbit, bs2;
+
+	if (n == 0)
+		_putchar('0');
+
+	for (bs2 = 1; n >= bs2; bs2 *= 2)
+	{
+		startbit = bs2;
+	}
+
+	for (; startbit >= 1; startbit /= 2)
+	{
+		if (n >= startbit)
+		{
+			_putchar('1');
+			n = n - startbit;
+		}
+		else
+			_putchar('0');
+	}
+}
