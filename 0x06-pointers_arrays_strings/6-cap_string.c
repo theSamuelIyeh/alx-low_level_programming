895f8f36a2f6427dc390aa09856a31afc29c1a25
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-11-19 11:27:39 -0800

    Another commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,6 +13,20 @@ char *cap_string(char *c)
 {
 	int i, increment;
 
+	if (c[0] >= 97 && c[0] <= 122)
+	{
+		c[0] = 65 + (c[0] - 97);
+	}
+
+	for (i = 1; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 65 && c[i] <= 90)
+		{
+			increment = c[i] - 65;
+			c[i] = 97 + increment;
+		}	
+	}
+
 	for (i = 0; c[i] != '\0'; i++)
 	{
 		if (c[i] == ' ' || c[i] == '\t' || c[i] == '\n' || c[i] == '.'
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main(void)
 {
-	char b[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	char b[] = "HELLO WORLD! expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
 	char *ptr;
 	ptr = cap_string(b);
 	printf("%s", b);
