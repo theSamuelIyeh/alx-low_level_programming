5b170916d025c8e78b19829d7c5eb8e2740bbd60
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-12-22 08:21:36 +0000

    Another commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,18 +12,41 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	char *name1 = name;
-	char *owner1 = owner;
+	int i = 0, j = 0, k;
+
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
 
 	new_dog = malloc(sizeof(dog_t));
-	if (new_dog != NULL)
+	if (new_dog == NULL)
 	{
-		new_dog->name = name1;
-		new_dog->age = age;
-		new_dog->owner = owner1;
-
-		return (new_dog);
+		free(new_dog);
+		return (NULL);
 	}
-	free(new_dog);
-	return (NULL);
+	new_dog->name = malloc(i * sizeof(new_dog->name));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+
+	for (k = 0; k <= i; k++)
+		new_dog->name[k] = name[k];
+	new_dog->age = age;
+	new_dog->owner = malloc(j * sizeof(new_dog->owner));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->owner);
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	for (k = 0; k <= j; k++)
+	{
+		new_dog->owner[k] = owner[k];
+	}
+	return (new_dog);
 }
