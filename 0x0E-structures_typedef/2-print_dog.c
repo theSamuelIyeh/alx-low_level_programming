043cc6290564195d65ea8e54121db7ae422a6a3f
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-12-22 07:21:31 +0000

    Another commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,14 +12,14 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d->name == NULL)
+		if (!d->name)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
 
 		printf("Age: %f\n", d->age);
 
-		if (d->owner == NULL)
+		if (!d->owner)
 			printf("Owner: (nil)");
 		else
 			printf("Owner: %s\n", d->owner);
