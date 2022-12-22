681b379c29f211990dd2343641a06167092a672d
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-12-22 06:53:04 +0000

    Another commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - function that initializes struct
+ * @d: structure object passed to function
+ * @name: name of dog to be initialised
+ * @age: age of dog to be initialised
+ * @owner: owner of dog to be initialised
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
