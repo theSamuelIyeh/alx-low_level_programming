cc498de29d26fb37d44af62018a50c69702a321f
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-12-22 06:47:25 +0000

    Another commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - struct dog
+ *
+ * @name: string of dog name
+ * @age: float age of dog
+ * @owner: string of owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
