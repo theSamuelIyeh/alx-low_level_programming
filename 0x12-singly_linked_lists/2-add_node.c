8c7b1d2bb8183bbf9f18bcdb42655c9250968df5
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-12-23 14:30:14 +0000

    Another commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,6 +20,11 @@ list_t *add_node(list_t **head, const char *str)
 	if (head == NULL)
 	{
 		ptr = malloc(sizeof(list_t));
+		if (ptr == NULL)
+		{
+			printf("Error\n");
+			exit(90);
+		}
 		head = &ptr;
 		ptr->str = strdup(str);
 		ptr->len = i;
@@ -27,6 +32,11 @@ list_t *add_node(list_t **head, const char *str)
 		return (ptr);
 	}
 	ptr = malloc(sizeof(list_t));
+	if (ptr == NULL)
+	{
+		printf("Error\n");
+		exit(90);
+	}
 	ptr->str = strdup(str);
 	ptr->len = i;
 	ptr->next = *head;
